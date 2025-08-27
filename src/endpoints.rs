3623745c7ab2055eee19788dd843375fd1e45743
JohnSwiftC <johnswift@live.com>
2025-08-27 09:21:10 -0400

    some basic api stuff

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,3 +1,24 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Deserialize)]
+pub struct CreateUserRequest {
+    email: String,
+    password: String,
+    linkedin: String,
+}
+
+#[derive(Deserialize)]
+pub struct ObtainJWTRequest {
+    email: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+pub struct VerifyJWTClaimsResponse {
+    claims: HashMap<String, String>,
+}
+
 pub async fn get_root() -> &'static str {
     "API is up!"
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    http::{HeaderMap, StatusCode},
     routing::{get, post},
     Router,
 };
