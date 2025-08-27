48745d9a9b9c6e29b6d2b9ec1336b3cb3dda5686
JohnSwiftC <johnswift@live.com>
2025-08-27 09:01:52 -0400

    initial commit, setting up axum

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+mod endpoints;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+        .route("/", get(endpoints::get_root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.expect("Failed to open socket");
+
+    axum::serve(listener, router).await.unwrap();
+}
