295a9921274fa3811249c2b6b4f4800b411fa3d5
JohnSwiftC <johnswift@live.com>
2025-08-29 10:28:34 -0400

    manual api hell

--- src/oauth.rs
+++ src/oauth.rs
@@ -51,6 +51,36 @@ pub struct LinkedInCallback {
     state: Option<String>,
 }
 
+#[derive(Deserialize)]
+pub struct LinkedInTokenResponse {
+    access_token: String,
+    expires_in: u64,
+    token_type: String,
+}
+
+#[derive(Deserialize)]
+pub struct LinkedInProfile {
+    id: String,
+    #[serde(rename = "localizedFirstName")]
+    first_name: String,
+    #[serde(rename = "localizedLastName")]
+    last_name: String,
+}
+
+#[derive(Deserialize)]
+pub struct LinkedInProfilePicture {
+    #[serde(rename = "profilePicture")]
+    profile_picture: ProfilePictureInfo,
+}
+
+#[derive(Deserialize)]
+pub struct ProfilePictureInfo {
+    #[serde(rename = "displayImage")]
+    display_image: String,
+}
+
+use reqwest::Client;
+
 pub async fn linkedin_callback(
     State(app_state): State<Arc<AppState>>,
     Query(params): Query<LinkedInCallback>,
@@ -58,8 +88,35 @@ pub async fn linkedin_callback(
     // TODO
     // Get LinkedIn name, photo, attach to user in supabase
 
-    // at this point, the browser should have a valid jwt, so connect that auth header
-    // to the linkedin info given here
+    let client = Client::new();
+    
+    let token_params = [
+        ("grant_type", "authorization_code"),
+        ("code", &params.code),
+        ("redirect_uri", "http://localhost:3000/auth/callback"),
+        ("client_id", &app_state.l_config.client_id),
+        ("client_secret", &app_state.l_config.client_secret),
+    ];
+    
+    let token_response = client
+        .post("https://www.linkedin.com/oauth/v2/accessToken")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .form(&token_params)
+        .send()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    
+    let token_data: LinkedInTokenResponse = token_response
+        .json()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    
+    let profile_response = client
+        .get("https://api.linkedin.com/v2/people/~:(id,localizedFirstName,localizedLastName)")
+        .header("Authorization", format!("Bearer {}", token_data.access_token))
+        .send()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     let redirect_html = format!(
         r#"
